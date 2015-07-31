cb34c09e45458ebf906e0156888f8e65d187a6f7
daryasary <hs.ramezanpoor@gmail.com>
2015-07-31 11:32:15 +0430

    Models registerd to admin site

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 import models
 # Register your models here.
 admin.site.register(models.Tag)
+admin.site.register(models.Category)
+admin.site.register(models.Post)
+admin.site.register(models.Comment)
