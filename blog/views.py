e62d5fb0776183028dda2cb5ac2126c36b9c75da
daryasary <hs.ramezanpoor@gmail.com>
2015-08-07 17:27:42 +0430

    Single page view improved

--- blog/views.py
+++ blog/views.py
@@ -35,9 +35,17 @@ def blog_list(request , cat=None, tag=None, year=None, Month=None):
 def single_post(request, slug):
 	# here will retrieve context, 
 	# and then render the page
-	try:
-		post = Post.objects.get(slug=slug)
-		# return HttpResponse( 'good request for %s' %post_id)
-		return render(request, 'blog/single_post.html', {'post':post})
-	except:
-		return HttpResponse('bad requests for %s' %slug)
+	# try:
+	# 	post = Post.objects.get(slug=slug)
+	# 	# return HttpResponse( 'good request for %s' %post_id)
+	# 	return render(request, 'blog/single_post.html', {'post':post})
+	# except:
+	# 	return HttpResponse('bad requests for %s' %slug)
+	Context = {}
+	Categories = Category.objects.all()
+	Tags = Tag.objects.all()
+	post = get_object_or_404(Post, slug=slug)
+	Context['post'] = post
+	Context['Categories'] = Categories
+	Context['Tags'] = Tags
+	return render(request, 'blog/single_post.html', Context)
