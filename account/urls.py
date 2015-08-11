2928f7800ba46ecfd5891a206433ad4200f82a06
daryasary <hs.ramezanpoor@gmail.com>
2015-08-11 20:34:11 +0430

    Allow other users to login

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-	url(r'^login/$', views.login, name='login'),
+	url(r'^login/$', views.user_login, name='login'),
 	url(r'^register/$', views.register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
-def login(request):
-	# return HttpResponse('Here is login page')
-	return render(request, 'account/login.html')
+def user_login(request):
+	if request.method == 'POST':
+		# return HttpResponse('YOU ARE LOGGED IN')
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(username=username, password=password)
+		if user:
+			if user.is_active:
+				login(request, user)
+				return HttpResponse('Success!!')
+			else:
+				return HttpResponse('USER IS NOT ACTIVE')
+		else:
+			return HttpResponse('Wrong username and password %s, %s' %(username, password))
+
+	else:
+		return render(request, 'account/login.html')
 
 def register(request):
 	return render(request, 'account/register.html')
