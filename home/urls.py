3917e1bbceeed330311ca802b1b64a0281a7f00d
hosein <hs.ramezanpoor@gmail.com>
2017-10-25 11:22:18 +0330

    Serve media files in development env

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from . import views as home
 
@@ -13,3 +15,5 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^account/', include('account.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
