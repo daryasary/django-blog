768097ca807599979eb5c0f3dfc6d088495ce022
hosein <hs.ramezanpoor@gmail.com>
2018-11-18 12:00:42 +0330

    Make templates localize

--- home/urls.py
+++ home/urls.py
@@ -5,10 +5,6 @@ from django.contrib import admin
 from . import views as home
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'home.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    # url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^redactor/', include('redactor.urls')),
     url(r'^$', home.index, name = 'home'),
